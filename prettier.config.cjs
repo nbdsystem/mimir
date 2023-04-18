8aae6812d59bc33f4e266b12b29681ae55bacfed
Josh Black <joshblack@github.com>
2023-04-17 21:21:03 -0500

    chore(project): add prettier-plugin-tailwindcss

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,9 +1,16 @@
-"use strict";
+'use strict';
+
+const path = require('path');
 
 module.exports = {
   singleQuote: true,
-  trailingComma: "all",
-  importOrder: ["^node:(.*)$", "<THIRD_PARTY_MODULES>", "^[./]"],
+  trailingComma: 'all',
+  importOrder: ['^node:(.*)$', '<THIRD_PARTY_MODULES>', '^[./]'],
   importOrderSeparation: false,
   importOrderSortSpecifiers: true,
+  plugins: [
+    require('@trivago/prettier-plugin-sort-imports'),
+    require('prettier-plugin-tailwindcss'),
+  ],
+  tailwindConfig: path.resolve(__dirname, './www/tailwind.config.cjs'),
 };
